be1b8959a909313ae57abfd4267a32b0e7cc8918
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-10-19 20:42:11 +0900

    improve sub op

--- source/orelang/operator/SubOperator.d
+++ source/orelang/operator/SubOperator.d
@@ -9,6 +9,10 @@ class SubOperator : IOperator {
    */
   public Value call(Engine engine, Value[] args) {
     Value ret = engine.eval(args[0]);
+    
+    if (args.length == 1) {
+      ret.mulTo(new Value(-1));
+    }
 
     foreach (arg; args[1..$]) {
       Value v = engine.eval(arg);
