d33e1c5822aa98ea27d6b42ea635205bf427d86a
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-11-13 20:50:09 +0900

    impl class op

---
+++ source/orelang/operator/ClassOperators.d
@@ -0,0 +1,54 @@
+module orelang.operator.ClassOperators;
+import orelang.operator.DynamicOperator,
+       orelang.operator.ClassType,
+       orelang.operator.IOperator,
+       orelang.Closure,
+       orelang.Engine,
+       orelang.Value;
+
+class ClassOperator : IOperator {
+  public Value call(Engine engine, Value[] args) {
+    string className = engine.eval(args[0]).getString;
+    Engine cEngine   = engine.clone;
+
+    if (args.length > 1) {
+      foreach (member; args[1..$]) {
+        cEngine.eval(member);
+      }
+    }
+
+    if ("constructor" !in cEngine.variables.called) {
+      cEngine.defineVariable("constructor", new Value(cast(IOperator)(new DynamicOperator((string[]).init, new Value)))); 
+    }
+
+    ClassType clst = new ClassType(className, cEngine);
+    Value cls      = new Value(clst);
+    engine.defineVariable(className, cls);
+
+    return cls;
+  }
+}
+
+
+class NewOperator : IOperator {
+  public Value call(Engine engine, Value[] args) {
+    string className;
+    if (args[0].type == ValueType.SymbolValue) {
+      className = args[0].getString;
+    } else {
+      className = engine.eval(args[0]).getString;
+    }
+
+    ClassType _cls = engine.getVariable(className).getClassType;
+    ClassType cls = new ClassType(_cls.className, _cls._engine.clone);
+
+    Value[] cArgs;
+    if (args.length > 0) {
+      cArgs = args[1..$];
+    }
+    //cls.eval(cArgs);
+    cls._engine.variables["constructor"].getIOperator.call(cls._engine, cArgs);
+
+    return new Value(cls);
+  }
+}
---
+++ source/orelang/operator/ClassType.d
@@ -0,0 +1,31 @@
+module orelang.operator.ClassType;
+import orelang.Engine,
+       orelang.Value;
+
+class ClassType {
+  string className;
+  Engine _engine;
+
+  this(string className, Engine _engine) {
+    this.className = className;
+    this._engine   = _engine;
+    _engine.sync_storage = true;
+  }
+
+  public Value call(Engine engine, Value[] args) {
+    string funcName;
+    if (args[0].type == ValueType.SymbolValue) {
+      funcName = args[0].getString;
+    } else {
+      funcName = engine.eval(args[0]).getString;
+    }
+
+    Value member = this._engine.variables[funcName];
+
+    if (member.type == ValueType.IOperator) {
+      return member.getIOperator.call(this._engine, args.length > 1 ? args[1..$] : (Value[]).init);
+    } else {
+      return member;
+    }
+  }
+}
