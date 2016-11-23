51d18ec1674fa05e8d361648290a4c498d3f93de
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-11-23 14:39:24 +0900

    add regex class

---
+++ source/orelang/operator/RegexClass.d
@@ -0,0 +1,51 @@
+module orelang.operator.RegexClass;
+import orelang.expression.ClassType,
+       orelang.operator.IOperator,
+       orelang.Engine,
+       orelang.Value;
+
+import std.stdio,
+       std.conv;
+import std.regex;
+class RegexClass : ClassType {
+  Regex!char rgx;
+
+  this (Engine _engine) {
+    _engine = _engine.clone;
+    super("Regex", _engine);
+
+    _engine.defineVariable("constructor", new Value(cast(IOperator)(
+      new class () IOperator {
+        public Value call(Engine engine, Value[] args) {
+          string pattern = engine.eval(args[0]).getString;
+
+          rgx = regex(pattern);
+
+          return new Value;
+        }
+      })));
+
+    _engine.defineVariable("reset", new Value(cast(IOperator)(
+      new class () IOperator {
+        public Value call(Engine engine, Value[] args) {
+          string pattern = engine.eval(args[0]).getString;
+
+          rgx = regex(pattern);
+
+          return new Value;
+        }})));
+
+    _engine.defineVariable("match", new Value(cast(IOperator)(
+      new class () IOperator {
+        public Value call(Engine engine, Value[] args) {
+          return new Value(cast(bool)(engine.eval(args[0]).getString.match(rgx)));
+        }})));
+
+    _engine.defineVariable("match-all", new Value(cast(IOperator)(
+      new class () IOperator {
+        public Value call(Engine engine, Value[] args) {
+          writeln(engine.eval(args[0]).getString.matchAll(rgx));
+          return new Value;
+        }})));
+  }
+}
