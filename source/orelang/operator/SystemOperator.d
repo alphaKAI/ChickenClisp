12e0a40be6f7cfed19a83d327ce7e317d6bac8f0
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2017-10-16 14:51:48 +0900

    add system

---
+++ source/orelang/operator/SystemOperator.d
@@ -0,0 +1,28 @@
+module orelang.operator.SystemOperator;
+import orelang.operator.IOperator,
+       orelang.Engine,
+       orelang.Value;
+
+import std.process,
+       std.string;
+
+class SystemOperator : IOperator {
+  /**
+   * call
+   */
+  public Value call(Engine engine, Value[] args) {
+    string[] args_strs;
+    
+    foreach (arg; args) {
+      args_strs ~= engine.eval(arg).toString;
+    }
+
+    try {
+      auto pid = spawnProcess(args_strs);//execute(args_strs.join(" "));
+
+      return new Value(wait(pid));
+    } catch {
+      return new Value(-1);
+    }
+  }
+}
